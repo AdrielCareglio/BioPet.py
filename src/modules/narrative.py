da8a201c6d34320b35f8acb15c8f402e645fea19
AdrielCareglio <adrielcaregliodev@gmail.com>
2025-03-01 13:36:49 +0100

    Update narrative.py with new changes

---
+++ src/modules/narrative.py
@@ -0,0 +1,11 @@
+"""
+Class that introduces the main narrative of the game.
+Uses ASCII formatted text and art to create an immersive experience.
+"""
+
+class Narrative:
+
+    def intro(self):
+        print(f"{YELLOW} After a long day, you're relaxed at home watching TV.{RESET}")
+
+
