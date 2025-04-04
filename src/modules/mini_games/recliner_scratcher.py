93b327d5aa09f162d21e00635f1fa840e7428cb0
AdrielCareglio <adrielcaregliodev@gmail.com>
2025-04-04 18:26:59 +0200

    Feature: finished recliner scratcher minigame and stated global for lives

--- src/modules/mini_games/recliner_scratcher.py
+++ src/modules/mini_games/recliner_scratcher.py
@@ -20,16 +20,16 @@ def scratcher_game(chosen_cat, op_cat, m_cat_color, op_cat_color):
     return state.lives_left
 
 def recliner_instructions(op_cat, op_cat_color):
-    print(f"{ITALICA}{op_cat_color}: -{op_cat}It´s time to face our worst enemy, the recliner. When we lean on it´s back to scratch and sharpen our claws, it leans back to scares us. WE'VE GOT TO GIVE IT WHAT IT DESERVES .'{RESET}")
+    print(f"{ITALICA}{op_cat_color}-{op_cat}: It´s time to face our worst enemy, the recliner. When we lean on it´s back to scratch and sharpen our claws, it leans back to scares us. WE'VE GOT TO GIVE IT WHAT IT DESERVES .'{RESET}")
 
-    print(f"{ITALICA}{op_cat_color}: -{op_cat} You must scratch the recliner. Be careful, we might get caught! We'll have to play it cool when the humans come. LET´S GO!{RESET}")
+    print(f"{ITALICA}{op_cat_color}-{op_cat}: You must scratch the recliner. Be careful, we might get caught! We'll have to play it cool when the humans come. LET´S GO!{RESET}")
 
-    print(f"{YELLOW} Press {CYAN}'Enter'{YELLOW} to {CYAN}scratch{YELLOW} and {MAGENTA}meow{YELLOW} to {MAGENTA}play it cool{RESET}. ")
+    print(f"Press {CYAN}'Enter'{RESET} to {CYAN}scratch{RESET} and {MAGENTA}meow{RESET} to {MAGENTA}play it cool{RESET}. ")
 
 
 def scratcher_dynamics(chosen_cat, op_cat, m_cat_color, op_cat_color):
 
-    print(f"{YELLOW}You have 20 seconds and {state.lives_left} lives. The countdown starts when you press {CYAN}Enter{YELLOW}. Good luck!")
+    print(f"You have 20 seconds and {state.lives_left} lives. The countdown starts when you press {CYAN}Enter{RESET}. Good luck!")
     input()
 
     active = True
@@ -45,15 +45,14 @@ def scratcher_dynamics(chosen_cat, op_cat, m_cat_color, op_cat_color):
 
         #End game if time runs out
         if current_time - start_time >= TIME_LIMIT:
-            print(f"{YELLOW}No time left!{RESET}")
+            print(f"{RESET}No time left!")
             active = False
 
         elif current_time >= next_interrupt:
-            print(f"{op_cat_color}{ITALICA}Someone´s coming! Type 'meow' and press 'Enter' to play it cool{RESET}")
+            print(f"{MAGENTA}{ITALICA}Someone´s coming! Type 'meow' and press 'Enter' to play it cool{RESET}")
             user_input = input().strip()
 
             if user_input == "meow":
-                print(f"{op_cat_color}{ITALICA}Great, keep going!{RESET}")
                 meow_count += 1
                 next_interrupt = time.time() + random.randint(1,5)
 
@@ -66,22 +65,22 @@ def scratcher_dynamics(chosen_cat, op_cat, m_cat_color, op_cat_color):
                     active = False
 
         else:
-            print(f"{op_cat_color}{ITALICA}Scratch now! (Press Enter){RESET}")
+            print(f"{CYAN}{ITALICA}Scratch now! (Press Enter){RESET}")
             user_input = input()
 
             if user_input == "":
                 scratch_count += 1
             else:
                 state.lives_left -= 1
-                print(f"{YELLOW}Press blank 'Enter', -1 life. Lives remaining: {state.lives_left}")
+                print(f"{RED}Press blank 'Enter', -1 life. Lives remaining: {state.lives_left}")
 
                 if state.lives_left == 0:
-                    print(f"{YELLOW}OH NO! You´re out of lives.{RESET}")
+                    print(f"{RED}OH NO! You´re out of lives.{RESET}")
                     active = False
 
     if state.lives_left > 0:
         print(f"{op_cat_color}{ITALICA}{op_cat}: Woho! We did it! The recliner won´t scare us again any time soon!{RESET}")
-        print(f"{YELLOW}Total scratches: {scratch_count}, succesful distractions: {meow_count} and lives left: {state.lives_left}")
+        print(f"{RESET}Total scratches: {scratch_count}, succesful distractions: {meow_count} and lives left: {state.lives_left}")
 
         end_message = ("You´re a natural, " if scratch_count >= 20 else "Not bad, ")
         print(op_cat_color+ITALICA+ end_message + chosen_cat + RESET)
